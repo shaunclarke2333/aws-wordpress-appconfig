b55a0c5f80a96a4c2ba4c9fe10a7f87514735e35
Shaun Clarke <shaunclarke43@gmail.com>
2022-08-11 16:40:46 -0700

    updated wordpress url

--- wp-config.php
+++ wp-config.php
@@ -87,7 +87,7 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
-/* Add any custom values between this line and the "stop editing" line. */
+/* Add any custom values domain_name_here between this line and the "stop editing" line. */
 
 
 
